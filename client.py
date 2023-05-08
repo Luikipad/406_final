ab2069c82e750df7a1a5364cf5e1c839225bf57f
Luikipad <btruong2@buffalo.edu>
2023-05-08 13:27:26 -0400

    Working Function

--- client.py
+++ client.py
@@ -7,11 +7,16 @@ app = Flask(__name__)
 Bootstrap(app)
 
 @app.route('/')
+def greeting():
+   return render_template('greeting.htm')
+
+@app.route('/home')
 def home():
    return render_template('home.htm')
 
+
 @app.route('/enternew')
-def new_student():
+def new_client():
    return render_template('client.htm')
 
 
@@ -25,9 +30,9 @@ def addrec():
          ClientWatch = request.form['ClientWatch']
          TotalSpent = request.form['TotalSpent']
          
-         with sql.connect(host="localhost", user="flask", password="password", database="flask_db") as con:
+         with sql.connect(host="localhost", user="flask", password="password", database="client_db") as con:
             cur = con.cursor()
-            cmd = "INSERT INTO employee (ClientID,ClientName,ClientPhone,ClientWatch,TotalSpent) VALUES ('{0}','{1}','{2}','{3}','{4}')".format(ClientID,ClientName,ClientPhone,ClientWatch,TotalSpent)
+            cmd = "INSERT INTO clients (ClientID,ClientName,ClientPhone,ClientWatch,TotalSpent) VALUES ('{0}','{1}','{2}','{3}','{4}')".format(ClientID,ClientName,ClientPhone,ClientWatch,TotalSpent)
             cur.execute(cmd)
             
             con.commit()
@@ -44,7 +49,7 @@ def addrec():
 def list():
    with sql.connect(host="localhost", user="flask", password="password", database="client_db") as conn:  
       cur = conn.cursor()
-      cur.execute("select * from client")
+      cur.execute("SELECT * FROM clients")
       rows = cur.fetchall()
 
    return render_template("list.htm",rows = rows)
